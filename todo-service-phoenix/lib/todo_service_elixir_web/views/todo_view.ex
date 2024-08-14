39cb46bd190cc5c52cc337274f8a36e564c2d1fe
Christoph Kappel <christoph@unexist.dev>
2024-08-14 15:45:34 +0200

    Added views

---
+++ todo-service-phoenix/lib/todo_service_elixir_web/views/todo_view.ex
@@ -0,0 +1,18 @@
+defmodule TodoServiceElixirWeb.TodoView do
+  use TodoServiceElixirWeb, :view
+  alias TodoServiceElixirWeb.TodoView
+
+  def render("index.json", %{todos: todos}) do
+    %{data: render_many(todos, TodoView, "todo.json")}
+  end
+
+  def render("show.json", %{todo: todo}) do
+    %{data: render_one(todo, TodoView, "todo.json")}
+  end
+
+  def render("todo.json", %{todo: todo}) do
+    %{id: todo.id,
+      name: todo.name,
+      description: todo.description}
+  end
+end
